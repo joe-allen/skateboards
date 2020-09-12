54f198e8c5eda37fc7c766772939f633969cb9e6
Joe Allen <hi.joeallen@gmail.com>
2020-09-12 10:20:24 -0400

    using netlify fn + eleventy cache-assets

--- functions/skateboards/skateboards.js
+++ functions/skateboards/skateboards.js
@@ -1,29 +1,57 @@
 const chromium = require('chrome-aws-lambda');
 
-async function skateboard() {
+const skate = async () => {
+
 	const browser = await chromium.puppeteer.launch({
 		executablePath: await chromium.executablePath,
 		args: chromium.args,
 		defaultViewport: chromium.defaultViewport,
-		headless: false,
+		headless: true,
 	});
 
 	const url  = 'https://www.skatewarehouse.com/';
 	const page =  await browser.newPage();
-
+	const date = new Date();
+	const time = date.getTime();
 	await page.goto(url);
-	await page.screenshot({path: 'skateboards.jpg'})
+
+	let dailyDose = await page.evaluate(() => {
+		const note = document.querySelector('.daily_dose_img ~ .note').textContent;
+		const dose_price = document.querySelector('.daily_dose_img ~ .price').textContent;
+		const link = document.querySelector('.daily_dose_img').parentElement.getAttribute('href');
+
+		return {
+			note,
+			price: dose_price,
+			link
+		}
+	});
 
 	await browser.close();
-}
 
+	return {
+		daily: dailyDose
+	};
+
+	// LOOK INTO IMG HOSTING
+	// TO SEND IMAGE TO
+	// let img = await page.screenshot({
+	// 	encoding: 'binary',
+	// 	path:     `./src/images/skateboards_${time}.jpg`
+	// });
+	// return {
+	// 	img: `skateboards_${time}.jpg`,
+	// 	daily: dailyDose
+	// };
+}
 
 exports.handler = async (event, context) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
+		const subject = event.queryStringParameters.name || 'World'
+		const item = await skate();
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
+      body: JSON.stringify({ message: `Hello ${subject}`, item }),
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
--- src/_data/skateboards.js
+++ src/_data/skateboards.js
@@ -1,25 +1,62 @@
-const chromium = require('chrome-aws-lambda');
+const Cache = require('@11ty/eleventy-cache-assets');
 
 module.exports = async () => {
+	try {
+		const { item } = await Cache(
+			'https://lucid-dijkstra-0e5a96.netlify.app/.netlify/functions/skateboards',
+			{
+				duration: '6h',
+				type: 'json'
+			}
+		);
 
-	const browser = await chromium.puppeteer.launch({
-		executablePath: await chromium.executablePath,
-		args: chromium.args,
-		defaultViewport: chromium.defaultViewport,
-		headless: true,
-	});
-
-	const url  = 'https://www.skatewarehouse.com/';
-	const page =  await browser.newPage();
-	const date = new Date();
-	const time = date.getTime();
-	await page.goto(url);
-	let img = await page.screenshot({
-		encoding: 'binary',
-		path:     `./src/images/skateboards_${time}.jpg`
-	});
-	await browser.close();
-
-	console.log('JOE', `skateboards_${time}.jpg`);
-	return `skateboards_${time}.jpg`;
+		return item;
+	} catch (error) {
+		return `There is an error b: ${error}`;
+	}
 }
+
+
+// const chromium = require('chrome-aws-lambda');
+// module.exports = async () => {
+
+// 	const browser = await chromium.puppeteer.launch({
+// 		executablePath: await chromium.executablePath,
+// 		args: chromium.args,
+// 		defaultViewport: chromium.defaultViewport,
+// 		headless: true,
+// 	});
+
+// 	const url  = 'https://www.skatewarehouse.com/';
+// 	const page =  await browser.newPage();
+// 	const date = new Date();
+// 	const time = date.getTime();
+// 	await page.goto(url);
+
+// 	// const userFeed = await page.waitForNavigation({waitUntil: 'networkidle0'});
+
+// 	let img = await page.screenshot({
+// 		encoding: 'binary',
+// 		path:     `./src/images/skateboards_${time}.jpg`
+// 	});
+
+// 	let dailyDose = await page.evaluate(() => {
+// 		const note = document.querySelector('.daily_dose_img ~ .note').textContent;
+// 		const dose_price = document.querySelector('.daily_dose_img ~ .price').textContent;
+// 		const link = document.querySelector('.daily_dose_img').parentElement.getAttribute('href');
+
+// 		return {
+// 			note,
+// 			price: dose_price,
+// 			link
+// 		}
+// 	});
+
+// 	await browser.close();
+
+// 	console.log('dailyDose', dailyDose);
+// 	return {
+// 		img: `skateboards_${time}.jpg`,
+// 		daily: dailyDose
+// 	};
+// }
